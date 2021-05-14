4b756e6beedeffe9546f5ef1159aa436353bbb65
Anu Goyal <example@example.com>
2021-05-14 22:08:51 +0100

    geo location

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geo_locator_app/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:intl/intl.dart';
@@ -58,6 +59,9 @@ class _HomepageState extends State<Homepage> {
        });
      });
      }
+     bool clickbutton= false;
+
+
 
 
 
@@ -89,55 +93,71 @@ class _HomepageState extends State<Homepage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              SizedBox(height:50),
-              DateRangePickerWidget(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height:50),
+                DateRangePickerWidget(),
 
-              SizedBox(height:50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Text("Checkin Date",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
-                      Text(DateFormat("dd/MM/yyyy",).format(DateTime.now()),style:TextStyle(fontSize: 15,)),
-                    ],
+                SizedBox(height:50),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //   children: [
+                //     Column(
+                //       children: [
+                //         Text("Checkin Date",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
+                //         Text(DateFormat("dd/MM/yyyy",).format(DateTime.now()),style:TextStyle(fontSize: 15,)),
+                //       ],
+                //     ),
+                //     SizedBox(width: 50,),
+                //     Column(
+                //       children: [
+                //         Text("Checkin time",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
+                //         Text(DateFormat("hh:mm").format(DateTime.now()),style:TextStyle(fontSize: 15,)),
+                //
+                //       ],
+                //     ),
+                //   ],
+                // ),
+                // SizedBox(height:30),
+                // Text("Location",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
+                // //Text("Location lat:${_position?.latitude??'-'},lon:${_position?.longitude??'-'}"),
+                // Text(locationMessage,style:TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+                // SizedBox(height:20),
+                // Text("Address from Coordinates",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
+                // SizedBox(height:20),
+                // Text("${_address?.addressLine??'-'}",style:TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+                SizedBox(height:50),
+                SingleChildScrollView(
+                  child: Container(
+                    height: 300,
+                    width: 400,
+                    child: widgettree(),
                   ),
-                  SizedBox(width: 50,),
-                  Column(
-                    children: [
-                      Text("Checkin time",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
-                      Text(DateFormat("hh:mm").format(DateTime.now()),style:TextStyle(fontSize: 15,)),
-
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height:30),
-              Text("Location",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
-              //Text("Location lat:${_position?.latitude??'-'},lon:${_position?.longitude??'-'}"),
-              Text(locationMessage,style:TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
-              SizedBox(height:20),
-              Text("Address from Coordinates",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
-              SizedBox(height:20),
-              Text("${_address?.addressLine??'-'}",style:TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
-              SizedBox(height:50),
-
-              //Text(DateFormat("dd/MM/yyyy",).format(DateTime.now()),style:TextStyle(fontSize: 15,)),
-              //SizedBox(height:50),
-              //Text("Checkin time",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
-              //Text(DateFormat("hh:mm").format(DateTime.now()),style:TextStyle(fontSize: 15,)),
-              //SizedBox(height:50),
-              // MaterialButton(
-              //   onPressed: (){
-              //     Navigator.push(context, MaterialPageRoute(builder: (_)=>DateRangePickerWidget()));
-              //   },
-              //
-              //     child: Text("attendence"))
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  onPressed: ()=> showdetails(),
+                  child: Text("Check In"),
+                ),
 
 
-            ],
+
+                //Text(DateFormat("dd/MM/yyyy",).format(DateTime.now()),style:TextStyle(fontSize: 15,)),
+                //SizedBox(height:50),
+                //Text("Checkin time",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,)),
+                //Text(DateFormat("hh:mm").format(DateTime.now()),style:TextStyle(fontSize: 15,)),
+                //SizedBox(height:50),
+                // MaterialButton(
+                //   onPressed: (){
+                //     Navigator.push(context, MaterialPageRoute(builder: (_)=>DateRangePickerWidget()));
+                //   },
+                //
+                //     child: Text("attendence"))
+
+
+              ],
+            ),
           ),
         ),
 
@@ -158,5 +178,17 @@ class _HomepageState extends State<Homepage> {
   Future<Address>convertCoordinatesToAddress(Coordinates coordinates)async {
     var addresses= await Geocoder.local.findAddressesFromCoordinates(coordinates);
     return addresses.first;
+  }
+
+  void showdetails() {
+    bool onclicked =false;
+    if(onclicked==true){
+        widgettree();
+    }
+
+    else{
+      return null;
+    }
+
   }
 }
